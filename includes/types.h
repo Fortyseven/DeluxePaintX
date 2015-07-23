206b110ef342ba3faa85f23a3f48591b69d3df46
Fortyseven <fortyseven@gmail.com>
2015-07-23 00:38:14 -0400

    Cleanup WIP

---
+++ includes/types.h
@@ -0,0 +1,20 @@
+#ifndef __TYPES__H__
+#define __TYPES__H__
+
+#include <stdlib.h>
+#include <stdbool.h>
+
+typedef bool BOOL;
+typedef short SHORT;
+typedef long LONG;
+typedef unsigned long ULONG;
+typedef unsigned short USHORT;
+typedef unsigned char UBYTE;
+typedef unsigned char BYTE;
+typedef unsigned short WORD;
+typedef unsigned short UWORD;
+
+#define TRUE true
+#define FALSE false
+
+#endif
